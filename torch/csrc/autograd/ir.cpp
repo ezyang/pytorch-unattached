d318e535b44a25e77f13debbeb55880b50d20108
Edward Z. Yang <ezyang@fb.com>
2017-06-26 11:51:16 -0700

    Don't use misleading Ret nomenclature.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/csrc/autograd/ir.cpp
+++ torch/csrc/autograd/ir.cpp
@@ -94,7 +94,7 @@ public:
   }
   void visitLocals(std::shared_ptr<Locals> e, int indent) {
     s << std::string(indent, ' ');
-    s << "Ret ";
+    s << "(";
     bool first = true;
     for (auto l : e->locals) {
       if (first) {
@@ -104,6 +104,7 @@ public:
       }
       visitLocal(l);
     }
+    s << ")";
   }
 };
 
