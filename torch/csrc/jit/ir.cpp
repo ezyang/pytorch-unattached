8e7fb2368b524cc8aae82b81255967f98f649ec9
Edward Z. Yang <ezyang@fb.com>
2017-07-19 08:45:28 -0700

    Simpler statement of structural invariants in lint.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- torch/csrc/jit/ir.cpp
+++ torch/csrc/jit/ir.cpp
@@ -5,9 +5,10 @@
 
 #include <iostream>
 #include <unordered_map>
-#include <unordered_set>
+#include <set>
 #include <stack>
 #include <sstream>
+#include <algorithm>
 
 namespace torch { namespace jit {
 
@@ -61,33 +62,57 @@ std::ostream& operator<<(std::ostream & out, Graph & g) {
   return out;
 }
 
+using node_set = std::set<Node*>;
+#define ALL_OF(container) container.begin(), container.end()
+
 // NB: Prefer using at() for bounds checking.
 void Graph::lint() const {
-  // Basic structural properties
-  std::unordered_set<Node*> known_nodes;
+  node_set all_nodes_set(ALL_OF(all_nodes));
+  node_set nodes_set(ALL_OF(nodes_));
+  node_set inputs_set(ALL_OF(inputs_));
+  node_set output_set {output_};
+
+  // no duplicates
+  JIT_ASSERT(all_nodes_set.size() == all_nodes.size());
+  JIT_ASSERT(nodes_set.size() == nodes_.size());
+  JIT_ASSERT(inputs_set.size() == inputs_.size());
+
+  // all_nodes must contain all of our other nodes
+  JIT_ASSERT(std::includes(ALL_OF(all_nodes_set), ALL_OF(nodes_set)));
+  JIT_ASSERT(std::includes(ALL_OF(all_nodes_set), ALL_OF(inputs_set)));
+  JIT_ASSERT(std::includes(ALL_OF(all_nodes_set), ALL_OF(output_set)));
+
+  // the only possible types of nodes are inputs, outputs and computation nodes
+  node_set sum_set;
+  sum_set.insert(ALL_OF(nodes_set));
+  sum_set.insert(ALL_OF(inputs_set));
+  sum_set.insert(ALL_OF(output_set));
+  JIT_ASSERT(std::includes(ALL_OF(sum_set), ALL_OF(all_nodes_set)));
+
+  // inputs, outputs and computation nodes are disjoint
+  JIT_ASSERT(std::find_first_of(ALL_OF(nodes_set), ALL_OF(inputs_set)) == nodes_set.end());
+  JIT_ASSERT(std::find_first_of(ALL_OF(nodes_set), ALL_OF(output_set)) == nodes_set.end());
+  JIT_ASSERT(std::find_first_of(ALL_OF(inputs_set), ALL_OF(output_set)) == inputs_set.end());
+
+  // all inputs should be Params, output should be Return
+  JIT_ASSERT(std::all_of(ALL_OF(inputs_set), [](Node* n) { return n->kind_ == NodeKind::Param; }));
+  JIT_ASSERT(std::all_of(ALL_OF(output_set), [](Node* n) { return n->kind_ == NodeKind::Return; }));
+  JIT_ASSERT(std::all_of(ALL_OF(nodes_set),  [](Node* n) {
+    return n->kind_ != NodeKind::Return && n->kind_ != NodeKind::Param;
+  }));
+
+  // per node properties
   for (auto& n : all_nodes) {
-    JIT_ASSERT(!known_nodes.count(n));
-    known_nodes.insert(n);
-  }
-  // TODO: nodes_ is a permutation of known_nodes
-  for (auto& n : nodes_) {
-    JIT_ASSERT(known_nodes.count(n));
-  }
-  std::unordered_set<Node*> known_inputs;
-  for (auto& n : inputs_) {
-    JIT_ASSERT(known_nodes.count(n));
-    JIT_ASSERT(!known_inputs.count(n));
-    JIT_ASSERT(n->kind_ == NodeKind::Param);
-    known_inputs.insert(n);
-  }
-  JIT_ASSERT(known_nodes.count(output_));
-  JIT_ASSERT(output_->kind_ == NodeKind::Return);
-  for (auto& n : all_nodes) {
+
+    // unique is the index into all_nodes
     JIT_ASSERT(all_nodes.at(n->unique_) == n);
+
+    // uses information is consistent
     for (auto& u : n->uses_) {
-      JIT_ASSERT(known_nodes.count(u.user));
+      JIT_ASSERT(all_nodes_set.count(u.user));
       JIT_ASSERT(u.user->inputs_.at(u.offset) == n);
     }
+
     // TODO: The select invariant (there is a unique select node for each
     // output of an op, if the node is a multi-return op).
     // See https://github.com/ezyang/pytorch/issues/8
