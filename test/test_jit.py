966caaa174fd7e72b55e3ea2aa38a0ef0608cbc2
Trevor Killeen <killeentm@gmail.com>
2017-08-16 11:52:01 -0700

    trace for dcgan

--- test/test_jit.py
+++ test/test_jit.py
@@ -935,6 +935,126 @@ class TestJit(TestCase):
         self.assertExpected(str(trace), "121")
         # self.assertExpected(torch._C._jit_pass_export(trace), "121-pbtxt")
 
+    def test_dcgan(self):
+
+        def weights_init(m):
+            classname = m.__class__.__name__
+            if classname.find('Conv') != -1:
+                m.weight.data.normal_(0.0, 0.02)
+            elif classname.find('BatchNorm') != -1:
+                m.weight.data.normal_(1.0, 0.02)
+                m.bias.data.fill_(0)
+
+        # configurable
+        bsz = 64
+        imgsz = 64
+        nz = 100
+        ngf = 64
+        ndf = 64
+        nc = 3
+
+        # Note, in both nets, bias should be false, inplace should true
+
+        class _netG(nn.Module):
+            def __init__(self, ngpu):
+                super(_netG, self).__init__()
+                self.ngpu = ngpu
+                self.main = nn.Sequential(
+                    # input is Z, going into a convolution
+                    nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=True),
+                    nn.BatchNorm2d(ngf * 8),
+                    nn.ReLU(False),
+                    # state size. (ngf*8) x 4 x 4
+                    nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=True),
+                    nn.BatchNorm2d(ngf * 4),
+                    nn.ReLU(False),
+                    # state size. (ngf*4) x 8 x 8
+                    nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=True),
+                    nn.BatchNorm2d(ngf * 2),
+                    nn.ReLU(False),
+                    # state size. (ngf*2) x 16 x 16
+                    nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=True),
+                    nn.BatchNorm2d(ngf),
+                    nn.ReLU(False),
+                    # state size. (ngf) x 32 x 32
+                    nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=True),
+                    nn.Tanh()
+                    # state size. (nc) x 64 x 64
+                )
+
+            def forward(self, input):
+                if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+                    output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+                else:
+                    output = self.main(input)
+                return output
+
+        class _netD(nn.Module):
+            def __init__(self, ngpu):
+                super(_netD, self).__init__()
+                self.ngpu = ngpu
+                self.main = nn.Sequential(
+                    # input is (nc) x 64 x 64
+                    nn.Conv2d(nc, ndf, 4, 2, 1, bias=True),
+                    nn.LeakyReLU(0.2, inplace=False),
+                    # state size. (ndf) x 32 x 32
+                    nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=True),
+                    nn.BatchNorm2d(ndf * 2),
+                    nn.LeakyReLU(0.2, inplace=False),
+                    # state size. (ndf*2) x 16 x 16
+                    nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=True),
+                    nn.BatchNorm2d(ndf * 4),
+                    nn.LeakyReLU(0.2, inplace=False),
+                    # state size. (ndf*4) x 8 x 8
+                    nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=True),
+                    nn.BatchNorm2d(ndf * 8),
+                    nn.LeakyReLU(0.2, inplace=False),
+                    # state size. (ndf*8) x 4 x 4
+                    nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=True),
+                    nn.Sigmoid()
+                )
+
+            def forward(self, input):
+                if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+                    output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+                else:
+                    output = self.main(input)
+
+                return output.view(-1, 1).squeeze(1)
+
+        # note, could have more than 1 gpu
+        netG = _netG(1)
+        netG.apply(weights_init)
+        netD = _netD(1)
+        netD.apply(weights_init)
+
+        input = torch.Tensor(bsz, 3, imgsz, imgsz)
+        noise = torch.Tensor(bsz, nz, 1, 1)
+        fixed_noise = torch.Tensor(bsz, nz, 1, 1).normal_(0, 1)
+        # label = torch.FloatTensor(bsz)
+        real_label = 1
+        fake_label = 0
+
+        fixed_noise = Variable(fixed_noise)
+
+        netD.zero_grad()
+        inputv = Variable(input)
+        # labelv = Variable(label)
+        # netD(inputv)
+
+        trace, _ = torch.jit.record_trace(netD, inputv)
+        print(str(trace))
+        self.assertExpected(str(trace), "dcgan-netD")
+        # self.assertExpected(torch._C._jit_pass_export(trace), "dcgan-netD-pbtxt")
+
+        noise.resize_(bsz, nz, 1, 1).normal_(0, 1)
+        noisev = Variable(noise)
+
+        trace, _ = torch.jit.record_trace(netG, noisev)
+        print(str(trace))
+        self.assertExpected(str(trace), "dcgan-netG")
+        # self.assertExpected(torch._C._jit_pass_export(trace), "dcgan-netG-pbtxt")
+
     def test_autograd_closure(self):
         a = x = Variable(torch.Tensor([0.4]), requires_grad=True)
         b = y = Variable(torch.Tensor([0.7]), requires_grad=True)
