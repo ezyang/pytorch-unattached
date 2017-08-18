346fcb4aed9721aeeb96322b51bd34dec03c5e27
Trevor Killeen <killeentm@gmail.com>
2017-08-18 15:17:12 -0700

    vgg test

---
+++ test/model_defs/caffe2_pytorch_test_vgg.py
@@ -0,0 +1,80 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np
+import sys
+import itertools
+from vgg import *
+
+try:
+    import caffe2
+except ImportError:
+    print('Cannot import caffe2, hence caffe2-torch test will not run.')
+    sys.exit(0)
+
+try:
+    import torch
+except ImportError:
+    print('Cannot import torch, hence caffe2-torch test will not run.')
+    sys.exit(0)
+
+import torch.nn as nn
+import torch.jit
+from torch.autograd import Variable, Function
+
+import toffee
+from toffee.backend import Caffe2Backend as c2
+
+import google.protobuf.text_format
+
+import unittest
+
+class TestCaffe2Backend(unittest.TestCase):
+    def test_vgg(self):
+        models = [make_vgg16(), make_vgg16_bn(), make_vgg19(), make_vgg19_bn()]
+        for underlying_model in models:
+            # Randomly but deterministically initialize the parameters (I hope!)
+            torch.manual_seed(0)
+            underlying_model.train(False)
+
+            # Random (deterministic) input
+            x = Variable(torch.randn(10, 3, 224, 224), requires_grad=True)
+
+            # Enable tracing on the model
+            trace, torch_out = torch.jit.record_trace(underlying_model, x)
+            proto = torch._C._jit_pass_export(trace)
+
+            graph_def = toffee.GraphProto()
+            google.protobuf.text_format.Merge(proto, graph_def)
+
+            # TODO: This is a hack; PyTorch should set it
+            graph_def.version = toffee.GraphProto().version
+
+            toffee.checker.check_graph(graph_def)
+
+            # Translate the parameters into Caffe2 form
+            W = {}
+            batch_norm_running_values = [s for s in graph_def.input if "saved" in s ]
+            real_inputs = [s for s in graph_def.input if "saved" not in s]
+            for v in batch_norm_running_values:
+                # print(v)
+                size = int(v.split('_')[-2])
+                if "mean" in v:
+                    W[v] = torch.zeros(size).numpy()
+                else:
+                    W[v] = torch.ones(size).numpy()
+            for k, v in zip(real_inputs, itertools.chain(underlying_model.parameters(), [x])):
+                W[k] = v.data.numpy()
+
+            caffe2_out_workspace = c2.run_graph(
+                init_graph=None,
+                predict_graph=graph_def,
+                inputs=W)
+            caffe2_out = list(caffe2_out_workspace.values())[0]
+            np.testing.assert_almost_equal(torch_out.data.numpy(), caffe2_out, decimal=3)
+
+
+if __name__ == '__main__':
+    unittest.main()
