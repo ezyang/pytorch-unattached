466c0a434d0120c6df9ba67f80a755bdab1bff64
Priya Goyal <priy2201@gmail.com>
2017-08-23 11:20:26 -0700

    E2E test with pre-trained model weights (#113)

--- test/model_defs/caffe2_pytorch_test_models.py
+++ test/model_defs/caffe2_pytorch_test_models.py
@@ -28,9 +28,9 @@ except ImportError:
     print('Cannot import torch, hence caffe2-torch test will not run.')
     sys.exit(0)
 
-# import torch.nn as nn
 import torch.jit
 from torch.autograd import Variable
+import torch.utils.model_zoo as model_zoo
 
 import toffee
 from toffee.backend import Caffe2Backend as c2
@@ -42,9 +42,20 @@ import unittest
 
 BATCH_SIZE = 2
 
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+    'densenet121': 'https://download.pytorch.org/models/densenet121-241335ed.pth',
+    'inception_v3_google': 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'squeezenet1_0': 'https://download.pytorch.org/models/squeezenet1_0-a815701f.pth',
+    'squeezenet1_1': 'https://download.pytorch.org/models/squeezenet1_1-f364aa15.pth',
+    'vgg16': 'https://download.pytorch.org/models/vgg16-397923af.pth',
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+}
+
 
 class TestCaffe2Backend(unittest.TestCase):
-    def run_model_test(self, model, train, batch_size):
+    def run_model_test(self, model, train, batch_size, state_dict=None):
         torch.manual_seed(0)
         model.train(train)
 
@@ -54,6 +65,7 @@ class TestCaffe2Backend(unittest.TestCase):
         # Enable tracing on the model
         trace, torch_out = torch.jit.record_trace(model, x)
         proto = torch._C._jit_pass_export(trace)
+        # print(proto)
 
         graph_def = toffee.GraphProto()
         google.protobuf.text_format.Merge(proto, graph_def)
@@ -65,14 +77,25 @@ class TestCaffe2Backend(unittest.TestCase):
 
         # Translate the parameters into Caffe2 form
         W = {}
-        batch_norm_running_values = [s for s in graph_def.input if "saved" in s]
-        real_inputs = [s for s in graph_def.input if "saved" not in s]
-        for v in batch_norm_running_values:
-            size = int(v.split('_')[-2])
-            if "mean" in v:
-                W[v] = torch.zeros(size).numpy()
-            else:
-                W[v] = torch.ones(size).numpy()
+        state_dict_running_vals = []
+        bn_running_values = [
+            s for s in graph_def.input if "saved_" in s]
+        if state_dict is not None:
+            # if we have the pre-trained model, use the running mean/var values
+            # from it otherwise use the dummy values
+            state_dict_running_vals = [
+                s for s in state_dict.keys() if "running_" in s]
+        else:
+            for v in bn_running_values:
+                size = int(v.split('_')[-2])
+                if "mean" in v:
+                    W[v] = torch.zeros(size).numpy()
+                else:
+                    W[v] = torch.ones(size).numpy()
+        real_inputs = [s for s in graph_def.input if "saved_" not in s]
+        for (v1, v2) in zip(bn_running_values, state_dict_running_vals):
+            print('v1: {} v2: {}'.format(v1, v2))
+            W[v1] = state_dict[v2].cpu().numpy()
         for k, v in zip(real_inputs, itertools.chain(model.parameters(), [x])):
             W[k] = v.data.numpy()
 
@@ -86,38 +109,62 @@ class TestCaffe2Backend(unittest.TestCase):
         print('Finished testing model.')
 
     def test_alexnet(self):
+        print('testing AlexNet model')
         alexnet = AlexNet()
+        alexnet.load_state_dict(model_zoo.load_url(model_urls['alexnet']))
         self.run_model_test(alexnet, train=False,
                             batch_size=BATCH_SIZE)
 
     def test_vgg(self):
-        models = [make_vgg16(), make_vgg19()]
+        print('testing VGG-16/19 models without BN')
+        vgg16 = make_vgg16()
+        vgg16.load_state_dict(model_zoo.load_url(model_urls['vgg16']))
+        vgg19 = make_vgg19()
+        vgg19.load_state_dict(model_zoo.load_url(model_urls['vgg19']))
+        models = [vgg16, vgg19]
+
         for underlying_model in models:
             self.run_model_test(underlying_model, train=False,
                                 batch_size=BATCH_SIZE)
 
     def test_vgg_bn(self):
+        print('testing VGG-16/19 models with BN')
         models = [make_vgg16_bn(), make_vgg19_bn()]
         for underlying_model in models:
             self.run_model_test(underlying_model, train=False,
                                 batch_size=BATCH_SIZE)
 
     def test_resnet(self):
+        print('testing ResNet50 model')
         resnet50 = ResNet(Bottleneck, [3, 4, 6, 3], inplace=False)
-        self.run_model_test(resnet50, train=False, batch_size=BATCH_SIZE)
+        state_dict = model_zoo.load_url(model_urls['resnet50'])
+        resnet50.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+
+        self.run_model_test(resnet50, train=False, batch_size=BATCH_SIZE,
+                            state_dict=state_dict)
 
     def test_squeezenet(self):
+        print('testing SqueezeNet1.1 model')
         sqnet_v1_1 = SqueezeNet(version=1.1, inplace=False)
-        self.run_model_test(sqnet_v1_1, train=False, batch_size=BATCH_SIZE)
+        state_dict = model_zoo.load_url(model_urls['squeezenet1_1'])
+        self.run_model_test(sqnet_v1_1, train=False, batch_size=BATCH_SIZE,
+                            state_dict=state_dict)
 
     def test_densenet(self):
+        print('testing DenseNet121 model')
         densenet121 = DenseNet(num_init_features=64, growth_rate=32,
                                block_config=(6, 12, 24, 16), inplace=False)
-        self.run_model_test(densenet121, train=False, batch_size=BATCH_SIZE)
+        # TODO: debug densenet for pretrained weights
+        # state_dict = model_zoo.load_url(model_urls['densenet121'])
+        self.run_model_test(densenet121, train=False, batch_size=BATCH_SIZE,
+                            state_dict=None)
 
     def test_inception(self):
+        print('testing Inception3 model')
         inception = Inception3(aux_logits=False, inplace=False)
-        self.run_model_test(inception, train=False, batch_size=BATCH_SIZE)
+        state_dict = model_zoo.load_url(model_urls['inception_v3_google'])
+        self.run_model_test(inception, train=False, batch_size=BATCH_SIZE,
+                            state_dict=state_dict)
 
 
 if __name__ == '__main__':
