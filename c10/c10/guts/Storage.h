4b5f0428f42fced2a2309a059e2f9103adb6ece4
Edward Z. Yang <ezyang@fb.com>
2018-04-10 14:36:36 -0400

    Noodling
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/guts/Storage.h
+++ c10/c10/guts/Storage.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <cstddef>
+
 namespace c10 { namespace guts {
 
 // Storage is NOT part of the public API
 class Storage {
-
+private:
+  void* data_;
+  ptrdiff_t size_;
+  // TODO: pack these boolean flags
+  // Is this storage resizable?  If it comes externally, or has been
+  // shared to some external system, it may not be.
+  bool resizable_;
+  // Should we free
+  bool
 };
 
 }} // namespace c10::guts
