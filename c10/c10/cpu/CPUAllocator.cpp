4d8b6b7826e43a456aceadacc557a8c821b648ed
Edward Z. Yang <ezyang@fb.com>
2018-04-11 15:37:37 -0400

    Fill in allocator code
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/cpu/CPUAllocator.cpp
+++ c10/c10/cpu/CPUAllocator.cpp
@@ -1,1 +1,15 @@
 #include "CPUAllocator.h"
+
+namespace c10 { namespace cpu {
+
+static std::unique_ptr<CPUAllocator> globalCPUAllocator(new SimpleCPUAllocator());
+
+CPUAllocator* getCPUAllocator() {
+  return globalCPUAllocator.get();
+}
+
+void setCPUAllocator(CPUAllocator* allocator) {
+  globalCPUAllocator.reset(allocator);
+}
+
+}}
--- c10/c10/cpu/CPUAllocator.h
+++ c10/c10/cpu/CPUAllocator.h
@@ -2,12 +2,23 @@
 
 #include <functional>
 
+namespace c10 { namespace cpu {
+
 struct CPUAllocator {
+  using data_t = std::unique_ptr<void, std::function<void(void*)>>;
+
   CPUAllocator() {}
   // In case the allocator wants to manage some internal state that needs
   // to be freed later
   virtual ~CPUAllocator() noexcept {}
-  //virtual std::unique_ptr<void, std::function<void(void*)>> malloc() {}
-  // virtual void realloc() {}
+  virtual data_t malloc(std::size_t) = 0;
+  // NB: Dropped getDeleter() for now
 };
 
+struct SimpleCPUAllocator : public CPUAllocator {
+  data_t malloc(std::size_t size) override {
+    return data_t(std::malloc(size), [](void* p) { std::free(p); });
+  }
+};
+
+}}
