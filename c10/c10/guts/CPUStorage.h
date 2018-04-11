4cd50163e0dbef041bc4c080432aa7f6746d8678
Edward Z. Yang <ezyang@fb.com>
2018-04-10 22:24:17 -0400

    CPUStorage all done
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/guts/CPUStorage.h
+++ c10/c10/guts/CPUStorage.h
@@ -7,6 +7,7 @@
 #include <functional>
 #include <cstdlib>
 #include <utility>
+#include <c10/Assert.h>
 
 namespace c10 { namespace guts {
 
@@ -21,10 +22,17 @@ namespace c10 { namespace guts {
 // In Caffe2, this was previously implemented directly as a std::shared_ptr.  Doing
 // it this means that realloc is not possible because a std::shared_ptr only
 // records the free() pointer.
+//
+// TODO: Consider making it possible to allocate CPUStorageImpl in the same block as a CPUTensor, so that
+// allocating a tensor is only one dynamic allocation rather than two
+//
+// Roughly corresponds to THStorage from old ATen
 class CPUStorageImpl {
+  using data_t = std::unique_ptr<void, std::function<void(void*)>>;
+
   // NB: THAllocator is axed; instead, all you can pass now is a custom deleter,
   // which is enough tod o the important things.
-  std::unique_ptr<void, std::function<void(void*)>> data_;
+  data_t data_;
 
   // NB: This is number of elements, NOT bytes
   // TODO: I'm not sure why we have to save this info
@@ -79,6 +87,15 @@ public:
   , resizable_(true)
   {}
 
+  // TODO: Make a more descriptive constructor for non-resizable things.  Note that since you're
+  // using make_shared most of the time for storages, a static method won't cut it.
+  CPUStorageImpl(data_t&& data, std::size_t element_size, std::size_t size, bool resizable=true)
+  : data_(std::move(data))
+  , element_size_(element_size)
+  , size_(size)
+  , resizable_(resizable)
+  {}
+
   // NB: Move constructor is legitimately used to destructively overwrite a storage, as in the case of a resize_()
   // TODO: explicitly declare permitted constructors.  (Consult my "rule of X" stuff...)
 
@@ -86,11 +103,13 @@ public:
 
   template <typename T>
   inline const T* data() const {
+    C10_ASSERT(sizeof(T) == scalar_size_);
     return static_cast<T*>(data_);
   }
 
   template <typename T>
   inline T* data() {
+    C10_ASSERT(sizeof(T) == scalar_size_);
     return static_cast<T*>(data_);
   }
 
@@ -107,19 +126,28 @@ public:
     return size_;
   }
 
-  // THStorage_(elementSize) ???
+  // THStorage_(elementSize)
+  // I'm... not really sure why we need to store this in here.
   inline std::size_t elementSize() const {
     return element_size_;
   }
 
+  // THStorage_(swap)
   // This is used to implement resize, which needs to "replace" a Storage.
   // NB: This can be used to cause memory unsafety, as size bounds stored in Tensors may become invalid.
   // NB: if you have a CPUStorage x, this is NOT the same thing as x.swap(y).  All that does is twiddle
-  // the shared pointers.  This actually swaps all the CONTENTS of the storage.
+  // the shared pointers.  This actually swaps all the CONTENTS of the storage.  This is why I didn't call
+  // it swap().
   void swap_contents(CPUStorageImpl& other) {
     // TODO: my IDE (clion) hates all uses of swap, for some reason
     std::swap(*this, other);
   }
+
+  // NB: deleted newWithSizeN variants
+  // NB: deleted setFlag/clearFlag
+  // NB: deleted retain/free
+  // NB: all of the new variants are sucked through the new constructor
+  // NB: deleted fill/set/get
 
 };
 
