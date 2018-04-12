58659743c69b884d589a9721130d48876384a6a1
Edward Z. Yang <ezyang@fb.com>
2018-04-12 16:00:51 -0400

    commentfix
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/Tensor.h
+++ c10/c10/Tensor.h
@@ -46,7 +46,7 @@ namespace c10 {
 //    - ssize_t is not actually signed on Windows, isn't part of the standard,
 //      and only guarantees that -1 is representable
 //    - long is still 32-bit on 64-bit Windows systems
-//    - ptrdiff_t is hypothetically OK, but it's not an intuitive name
+//    - ptrdiff_t is allowed to have 2**15-1 as its max value in C
 //
 // So, we have two choices: (1) we could define our OWN integer type (synonym
 // to ptrdiff_t), or (2) we can always use int64_t and eat the performance cost on
