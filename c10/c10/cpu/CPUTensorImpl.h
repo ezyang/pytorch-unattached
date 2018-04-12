91b3e0bce0a5976aa1f89822640dcc7edad5fe0f
Edward Z. Yang <ezyang@fb.com>
2018-04-12 11:23:03 -0400

    resize is done
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/cpu/CPUTensorImpl.h
+++ c10/c10/cpu/CPUTensorImpl.h
@@ -6,14 +6,19 @@
 
 namespace c10 { namespace cpu {
 
+// Everything is ssize_t to prevent us from accidentally doing a signed-unsigned operation
+// which is basically never what you want.  But using ssize_t instead of int64_t shuts
+// up the compiler about size_t conversions from standard library.
+
 class CPUTensorImpl final : public guts::TensorImpl {
-  std::size_t element_size_;
+  ssize_t element_size_bytes_;
   // Note: storage->size() may be greater than the recorded size of the tensor
+  // ezyang to @smessmer: Maybe we should consider using a never-null pointer
   CPUStorage storage_;
   // Note: In Torch this can be nonzero, because we support views into the
   // inside of tensors.  In historic Caffe2 this was always zero.
   // NB: This is BYTES!!!  Different from TH historically, which was scalar size.
-  std::size_t storage_offset_;
+  ssize_t storage_offset_bytes_;
   // NB: shares_data from Caffe2 was axed, because it is SOLELY used to determine
   // check what the overall tensor usage is.  We can rewrite that code to
   // keep a mapping of storage base pointers that it has seen (these all
@@ -35,14 +40,15 @@ class CPUTensorImpl final : public guts::TensorImpl {
   // See also https://ezyang.github.io/stride-visualizer/index.html
   SmallVector<int64_t, 5> stride_;
 public:
-  CPUTensorImpl(std::size_t element_size, const CPUStorage& storage)
+  CPUTensorImpl(ssize_t element_size_bytes, const CPUStorage& storage)
   : TensorImpl(TypeIds::CPUTensor)
-      , element_size_(element_size)
+      , element_size_bytes_(element_size_bytes)
       , storage_(storage)
   {};
 
   void *data_ptr() const override {
-    return static_cast<void*>(static_cast<char*>(storage_->data_ptr()) + storage_offset_);
+    if (!storage_) return nullptr;
+    return static_cast<void*>(static_cast<char*>(storage_->data_ptr()) + storage_offset_bytes_);
   }
 
   // Hacked up operators
@@ -67,10 +73,12 @@ public:
   // incorrectly assume the original tensor was
   // contiguously strided for every negative index, even when it was not.
   // See also https://github.com/pytorch/pytorch/issues/229
+  // NB: This does NOT deallocate memory when you resize to something smaller.
   void HACK_resize_(ArrayRef<int64_t> new_size, ArrayRef<int64_t> new_stride) override {
     C10_ASSERT(new_size.size() == new_stride.size());
     bool unchanged = new_size.equals(size()) && new_stride.equals(stride());
     if (unchanged) return;
+    // TODO: Refactor this into a utility
     auto compute_extent = [](ArrayRef<int64_t> size, ArrayRef<int64_t> stride) {
       // Watermarks are inclusive.  NB: watermarks can be negative! Careful!
       ssize_t high_watermark = 0;
@@ -86,8 +94,24 @@ public:
     };
     ssize_t low_watermark, high_watermark;
     std::tie(low_watermark, high_watermark) = compute_extent(new_size, new_stride);
+    // NB: Actually, we should be able to support resizing the left-side of the tensor; we
+    // simply need to support the case when the pointer doesn't point to the beginning
+    // of the allocated region; this means we need to store an offset to "undo" the
+    // shift later.  Should be simple but we don't implement it for now.
+    if (low_watermark * element_size_bytes_ + storage_offset_bytes_ < 0) {
+      throw std::runtime_error("Cannot resize past beginning of tensor");
+    }
     size_.assign(new_size.begin(), new_size.end());
     stride_.assign(new_stride.begin(), new_stride.end());
+    if (high_watermark * element_size_bytes_ + storage_offset_bytes_ > 0) {
+      if (!storage_) {
+        storage_ = std::make_shared<CPUStorageImpl>();
+      }
+      auto new_size_bytes = high_watermark * element_size_bytes_ + storage_offset_bytes_;
+      if (new_size_bytes > storage_->sizeBytes()) {
+        storage_->resize_(new_size_bytes);
+      }
+    }
   }
 };
 
