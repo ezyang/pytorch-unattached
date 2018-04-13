6961ee578f52d4e7199a1335de19a17ca146dacb
Edward Z. Yang <ezyang@fb.com>
2018-04-13 16:42:46 -0400

    Poor man's typemeta
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/guts/ScalarType.h
+++ c10/c10/guts/ScalarType.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <cstdint>
+#include <cstddef>
+#include <string>
 
 namespace c10 {
 
-// Modeled off of TypeMeta
+// Poor man's TypeMeta
 
 #define C10_FORALL_SCALAR_TYPES(_) \
 _(uint8_t,Byte,i) \
@@ -18,33 +20,80 @@ _(double,Double,d)
 // _(Half,Half,d) \
 
 class ScalarType {
+  // ezyang to @smessmer: Sorry, old school typedef ^^"
+  typedef void (*PlacementNew)(void*, size_t);
+  typedef void (*TypedCopy)(const void*, void*, size_t);
+  typedef void (*TypedDestructor)(void*, size_t);
+
   int64_t itemsize_;
+  const char* name_;
+  PlacementNew ctor_;
+  TypedCopy copy_;
+  TypedDestructor dtor_;
 public:
-  constexpr ScalarType(int64_t itemsize)
-      : itemsize_(itemsize) {}
+
+  constexpr ScalarType(int64_t itemsize, const char* name, PlacementNew ctor, TypedCopy copy, TypedDestructor dtor)
+  : itemsize_(itemsize)
+  , name_(name)
+  , ctor_(ctor)
+  , copy_(copy)
+  , dtor_(dtor)
+  {}
+
+  constexpr PlacementNew ctor() const { return ctor_; }
+  constexpr TypedCopy copy() const { return copy_; }
+  constexpr TypedDestructor dtor() const { return dtor_; }
 };
 
-enum class ScalarTypeId : int64_t {
-#define DEFINE_ENUM(_1,n,_2) \
-  n,
+/**
+ * Placement new function for the type.
+ */
+template <typename T>
+void _Ctor(void* ptr, size_t n) {
+  T* typed_ptr = static_cast<T*>(ptr);
+  for (int i = 0; i < n; ++i) {
+    new (typed_ptr + i) T;
+  }
+}
 
-  C10_FORALL_SCALAR_TYPES(DEFINE_ENUM)
-#undef DEFINE_ENUM
-};
+/**
+ * Destructor for non-fundamental types.
+ */
+template <typename T>
+void _Dtor(void* ptr, size_t n) {
+  T* typed_ptr = static_cast<T*>(ptr);
+  for (int i = 0; i < n; ++i) {
+    typed_ptr[i].~T();
+  }
+}
+
+/**
+ * Typed copy function for classes.
+ */
+template <typename T>
+void _Copy(const void* src, void* dst, size_t n) {
+  const T* typed_src = static_cast<const T*>(src);
+  T* typed_dst = static_cast<T*>(dst);
+  for (int i = 0; i < n; ++i) {
+    typed_dst[i] = typed_src[i];
+  }
+}
 
 namespace ScalarTypes {
-#define DEFINE_STATIC(_1,name,_2) \
-  constexpr ScalarType name(static_cast<int64_t>(ScalarTypeId::name));
+#define DEFINE_STATIC(ctype,name,_2) \
+  constexpr ScalarType name(sizeof(ctype), #name, nullptr, nullptr, nullptr);
 
 C10_FORALL_SCALAR_TYPES(DEFINE_STATIC)
 #undef DEFINE_STATIC
+
+  constexpr ScalarType String(sizeof(std::string), "String", _Ctor<std::string>, _Copy<std::string>, _Dtor<std::string>);
 }
 
-template <typename T> const ScalarType& mkScalarType();
+template <typename T> const ScalarType* mkScalarType();
 #define DEFINE_TEMPLATE(ctype,name,_1) \
 template <> \
-const ScalarType& mkScalarType<ctype>() { \
-  return ScalarTypes::name; \
+const ScalarType* mkScalarType<ctype>() { \
+  return &ScalarTypes::name; \
 }
 
 C10_FORALL_SCALAR_TYPES(DEFINE_TEMPLATE)
