5d28a48c80661f3f2a1b02105e5be9e3962cedfd
Edward Z. Yang <ezyang@fb.com>
2018-04-18 23:22:34 -0400

    rename
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/cpu/op/CPUAll_test.cpp
+++ c10/c10/cpu/op/CPUAll_test.cpp
@@ -8,7 +8,7 @@ using I = ArrayRef<int64_t>;
 static auto shapes = ::testing::Values(I{}, I{0}, I{1}, I{0,1}, I{1,1}, I{2,1}, I{2,2}, I{2,2,2});
 
 class CPUAll_empty_test : public ::testing::TestWithParam<ArrayRef<int64_t>> {};
-TEST_P(CPUAll_empty_test, basic) {
+TEST_P(CPUAll_empty_test, int32) {
   ArrayRef<int64_t> size = GetParam();
   constexpr DataType dtype = int32;
 
@@ -19,7 +19,7 @@ TEST_P(CPUAll_empty_test, basic) {
 INSTANTIATE_TEST_CASE_P(CPUAll_empty_test_shapes, CPUAll_empty_test, shapes);
 
 class CPUAll_zeros_test : public ::testing::TestWithParam<ArrayRef<int64_t>> {};
-TEST_P(CPUAll_zeros_test, basic) {
+TEST_P(CPUAll_zeros_test, int32) {
   ArrayRef<int64_t> size = GetParam();
   using dtype_t = int32_t;
   constexpr DataType dtype = c10::dtype<dtype_t>();
