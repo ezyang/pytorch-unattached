464cff4f374a5b2b6be56c6159d66bfb656da521
Edward Z. Yang <ezyang@fb.com>
2018-04-23 21:36:45 -0400

    buildfix
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/test/basic.cpp
+++ c10/test/basic.cpp
@@ -4,8 +4,7 @@ using namespace c10;
 
 // Just a scratch pad for running little programs
 int main() {
-  Tensor x = tensor(float32, {3, 2}, {2, 1});
-  x.resize_({5, 5}, {5, 1});
-
-  Tensor y = tensor<std::string>({1,2}, {"foo", "bar"});
+  Tensor x = empty({3, 2}, float32);
+  x.legacy_pytorch_resize_({5, 5});
+  Tensor y = tensor<std::string>({"foo", "bar"}, {1,2});
 }
