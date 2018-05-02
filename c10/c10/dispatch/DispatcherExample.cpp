1bc7d8fe4f6b7c218ff72d736d375b6249a253cc
Sebastian Messmer <messmer@fb.com>
2018-05-01 18:09:42 -0700

    Use new op registration in DispatcherExample

--- c10/c10/dispatch/DispatcherExample.cpp
+++ c10/c10/dispatch/DispatcherExample.cpp
@@ -1,16 +1,18 @@
 #include "Dispatcher.h"
 #include <c10.h>
 #include <c10/cpu/CPUTensorImpl.h>
+#include <c10/dispatch/OpRegistration.h>
 
 using namespace c10;
+using c10::cpu::CPU_TENSOR;
 
 namespace ops {
 struct conditional final {
-  static constexpr OpId op_id() {return OpId{0}; }
+  static constexpr OpId op_id() {return OpId{1000}; }
   using Signature = Tensor(bool, Tensor, Tensor);
 };
 struct add_notensor final {
-  static constexpr OpId op_id() {return OpId{1};}
+  static constexpr OpId op_id() {return OpId{1001};}
   using Signature = int(int, int);
 };
 }
@@ -23,16 +25,16 @@ Tensor conditional_op(bool condition, Tensor thenTensor, Tensor elseTensor) {
   }
 }
 
+C10_REGISTER_OP().define<ops::conditional>(&conditional_op, {CPU_TENSOR(), CPU_TENSOR()});
+
 int add_notensor_op(int lhs, int rhs) {
   return lhs + rhs;
 }
 
-using c10::cpu::CPU_TENSOR;
+C10_REGISTER_OP().define<ops::add_notensor>(&add_notensor_op);
 
 int main() {
   Dispatcher& d = Dispatcher::singleton();
-  d.registerOp<ops::conditional>(&conditional_op, {CPU_TENSOR(), CPU_TENSOR()});
-  d.registerOp<ops::add_notensor>(&add_notensor_op);
 
   Tensor t1 = tensor<int>({5});
   Tensor t2 = tensor<int>({10});
