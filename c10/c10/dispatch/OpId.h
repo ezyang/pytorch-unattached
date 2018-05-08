5b73882a078a297d90d2d2c5be1290af3fad7466
Edward Z. Yang <ezyang@fb.com>
2018-05-08 10:35:25 -0400

    OpId comment
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/dispatch/OpId.h
+++ c10/c10/dispatch/OpId.h
@@ -4,6 +4,13 @@
 
 namespace c10 {
 
+/**
+ * Unique identifier for an operator signature, e.g., identifies 'add' versus 'mul' distinctly.
+ *
+ * NB: Every distinct overload of an operator, e.g., mm(Tensor, Tensor)
+ * versus mm(double alpha, Tensor, double beta, Tensor), gets a DISTINCT
+ * OpId.
+ */
 class OpId final : public guts::IdWrapper<OpId, uint32_t> {
 public:
   // TODO Don't allow public constructor
