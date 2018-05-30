8b6c1e4862037ad515dea29f129307127f85f56e
Edward Z. Yang <ezyang@fb.com>
2018-05-30 10:57:51 -0400

    More notes on stack based operator
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/stack_bindings/StackBasedOperator.h
+++ c10/c10/stack_bindings/StackBasedOperator.h
@@ -24,17 +24,34 @@ private:
 
 
 /**
- * This class provides a virtual interface to operators using a boxed input representation.
- * Unlike direct-style, unboxed operators, we can work with stack based operators (and their
- * input/outputs) in a polymorphic way without requiring templates.
+ * The stack based operator interface takes it's arguments via a ParameterStack,
+ * and pushes its outputs onto the ParameterStack.  This makes it much easier to work with from
+ * client code which needs to operate polymorphically over operators which have different signatures.
+ *
+ * Example: Suppose that you have the operator 'minus' which subtracts two tensors and returns the result.
+ * Then supposing callStack is [c, b, a] upon entry, then on exit it will be [c, a - b] (c, in general,
+ * is some prefix of parameters on the stack which are untouched by the stack.)
  */
 class StackBasedOperator {
 public:
+  /**
+   * Invoke the operator, popping arguments from the top of the stack (first argument is very top)
+   * and pushing outputs onto the stack (only ever one argument; AT THE MOMENT).
+   * @param callStack
+   */
   virtual void operator()(ParameterStack* callStack) = 0;
 
   virtual ~StackBasedOperator() = default;
 };
 
+// TODO: This probably lives at the wrong level of abstraction now, given a change of plans
+// due to https://fb.quip.com/o5pXA1LHgAq0
+/**
+ * The ConcreteStackBasedOperator implements the wrapper for a specific operator schema, popping
+ * arguments off the stack into the correct unboxed form to subsequently call the dispatcher.
+ *
+ * @tparam OpSchemaDef The OpSchemaDef to create a ConcreteStackBasedOperator from.
+ */
 template<class OpSchemaDef>
 class ConcreteStackBasedOperator final : public StackBasedOperator {
 private:
@@ -59,6 +76,10 @@ public:
 
     // TODO Check if this correctly moves the arguments from the tuple into the op
     ReturnType result = details::call_dispatcher_with_args_tuple<OpSchemaDef, ReturnType, ArgumentsTuple>::call(std::move(arguments));
+    // TODO ezyang to smessmer: This looks questionable, it seems more likely that if we get a tuple back from
+    // the result, we will want to unpack it into the stack, because if we have an implementation of Any that
+    // has the small result optimization, this will help us having to avoid doing dynamic allocations to have space
+    // to store the result.
     callStack->push(std::move(result));
   }
 };
