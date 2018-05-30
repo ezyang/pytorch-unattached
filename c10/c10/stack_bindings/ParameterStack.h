9cf5b01a005de3a194c4c662619891ab29c7edd8
Edward Z. Yang <ezyang@fb.com>
2018-05-30 10:10:40 -0400

    Expunge more callStack_ references
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- c10/c10/stack_bindings/ParameterStack.h
+++ c10/c10/stack_bindings/ParameterStack.h
@@ -17,20 +17,20 @@ public:
   template<class T>
   void push(T&& arg) {
     // TODO Why is make_any in detail?
-    callStack_.push_back(c10::detail::make_any(std::forward<T>(arg)));
+    stack_.push_back(c10::detail::make_any(std::forward<T>(arg)));
   }
 
   template<class T>
   T pop() {
     static_assert(!std::is_reference<T>::value, "Cannot pop by reference");
     // TODO Make sure this moving out of the stack works correctly and doesn't copy
-    T result = std::move(callStack_.back()).get<T>();
-    callStack_.pop_back();
+    T result = std::move(stack_.back()).get<T>();
+    stack_.pop_back();
     return result;
   }
 
 private:
-  std::vector<Any> callStack_;
+  std::vector<Any> stack_;
 };
 
 }
