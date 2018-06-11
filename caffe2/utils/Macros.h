1f35c8f91f3bed897214e3d909bf49833c8c12c5
Sebastian Messmer <messmer@fb.com>
2018-06-11 15:27:51 -0700

    TensorTypeId

---
+++ caffe2/utils/Macros.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/**
+ * Disallow the copy and assignment constructors of a class
+ */
+#define DISALLOW_COPY_AND_ASSIGN(Class)         \
+  Class(const Class &rhs) = delete;             \
+  Class &operator=(const Class &rhs) = delete
